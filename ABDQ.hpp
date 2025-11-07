93a6c8ef4b03e81ffbf3dbb7f2e67513882cbc42
Fizi3d <fizi3d3@gmail.com>
2025-11-07 00:17:21 +0000

    m

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -20,7 +20,7 @@ public:
     // Big 5
     ABDQ() : data_(new T[4]), capacity_(4), size_(0), front_(0), back_(0){}
     explicit ABDQ(std::size_t capacity) : data_(new T[capacity]), capacity_(capacity), size_(0), front_(0), back_(0){}
-    ABDQ(const ABDQ& other) : data_(new T[other.capacity_]), capacity_(other.capacity_), size_(other.size_), front_(other.front_), back_(other.back_) {
+    ABDQ(const ABDQ& other) : data_(new T[other.capacity_]), capacity_(other.capacity_), size_(other.size_), front_(0), back_(other.size_) {
         for (size_t i = 0; i < other.size_; i++) {
             data_[i] = other.data_[(other.front_ + i) % capacity_];
         }
